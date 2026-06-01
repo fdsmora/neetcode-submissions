6230c082f66aa00ab711751d7fa4fca3e73e4181
Fausto Salazar Mora <fausto.ds.mora@gmail.com>
2026-06-01 09:25:43 -0600

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.py
@@ -0,0 +1,49 @@
+class Vertical:
+    def __init__(self, matrix: list[list[int]]):
+        R, C = len(matrix), len(matrix[0])
+        self.matrix = [[0]*C for _ in range(R)]
+        for j in range(C):
+            accum = 0
+            for i in range(R):
+                accum+=matrix[i][j]
+                self.matrix[i][j]=accum
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+        R, C = len(matrix), len(matrix[0])
+        self.prefix = [[0]*C for _ in range(R)]
+        for i in range(R):
+            accum = 0
+            for j in range(C):
+                accum += matrix[i][j]
+                #print(f"accum:{accum}")
+                self.prefix[i][j]=accum
+            #print(self.prefix[i])
+        self.vertical = Vertical(self.prefix)
+       # self.print(self.prefix)
+       # self.print(self.vertical.matrix)
+    
+    def print(self, M:list[list[int]]):
+        #print(M)
+        # R, C = len(M), len(M[0])
+        # for i in range(R):
+        #     print(M[i])
+        for row in M:
+            print(row)
+        print()
+
+
+
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        compensate = self.vertical.matrix[row1-1][col1-1] if row1>0 and col1>0 else 0
+        upperSum = self.vertical.matrix[row1-1][col2] if row1>0 else 0
+        leftSum = self.vertical.matrix[row2][col1-1] if col1>0 else 0
+        return self.vertical.matrix[row2][col2]-upperSum-leftSum+compensate
+
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
