a06dba2b42b58c5e626481347080c271c1fc8f81
Fausto Salazar Mora <fausto.ds.mora@gmail.com>
2026-06-11 20:24:19 -0600

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums)==1:
+            return True
+        goal = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if i+nums[i]>=goal:
+                goal=i
+
+        return goal==0
+
+
+        
+
